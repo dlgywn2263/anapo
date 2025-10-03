be4860f0941c30b7f00fb7f86adf73065db68a86
dlgywn2263 <lhj-3988@naver.com>
2025-10-03 20:54:16 +0900

    시작 페이지 수정

--- app/Landing/page.jsx
+++ app/Landing/page.jsx
@@ -3,7 +3,7 @@ import StartOption from "@/src/StartOption";
 
 const Page = () => {
   return (
-    <section className="relative h-screen w-screen   flex items-center justify-center">
+    <section className="relative h-screen  w-screen flex items-center justify-center">
       <Image src={"/anapo_main.jpg"} alt={""} fill className="object-cover" />
       <div className="relative z-10">
         <StartOption />
